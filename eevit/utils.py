c515351602941031fa80f6b9bbe968ff31ae4262
Jonathan Mikler <ionymikler@gmail.com>
2025-01-21 14:44:27 +0100

    replace inline operations in fast_pass utils

--- eevit/utils.py
+++ eevit/utils.py
@@ -7,22 +7,21 @@ def add_fast_pass(x):
     return torch.cat([x, torch.zeros(x.shape[0], 1, x.shape[-1])], dim=1)
 
 
-def remove_fast_pass(x_with_fastpass):
-    return x_with_fastpass[:, :-1, :]
+def remove_fast_pass(x_with_fastpass: torch.Tensor):
+    return x_with_fastpass.clone()[:, :-1, :]
 
 
-def get_fast_pass(x_with_fastpass):
-    return x_with_fastpass[:, -1, :]
+def get_fast_pass(x_with_fastpass: torch.Tensor):
+    return x_with_fastpass.clone()[:, -1, :]
 
 
-def flip_fast_pass_token(x_with_fastpass):
-    x_with_fastpass[:, -1, :] = 1.0
-    return x_with_fastpass
+def flip_fast_pass_token(x_with_fastpass: torch.Tensor):
+    output = x_with_fastpass.clone()
+    output[:, -1, :] = 1.0
+    return output
 
 
 def confidence(x):
-    # x: torch.Tensor, logits BEFORE softmax
     softmax = torch.softmax(x, dim=-1)
-    # print("WARNING: confidence is giving a dummy value")
     # return 0.6
     return torch.max(softmax)
